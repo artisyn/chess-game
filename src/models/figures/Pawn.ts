ca8a75e807f3eebb06e1304db6a3612716ff8900
artisyn <artioms1@gmail.com>
2022-06-25 18:20:23 +0300

    moving methods

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -4,11 +4,23 @@ import blackLogo from '../../assets/blackPawn.png';
 import whiteLogo from '../../assets/whitePawn.png';
 import { Colors } from '../Colors';
 import { Cell } from '../Cell';
+import { BlobOptions } from 'buffer';
 
 export class Pawn extends Figure {
+	isFirstStep: boolean = true;
 	constructor(color: Colors, cell: Cell) {
 		super(color, cell);
 		this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
 		this.name = FigureNames.PAWN;
 	}
+	canMove(target: Cell): boolean {
+		if (!super.canMove(target)) {
+			return false;
+		}
+		return true;
+	}
+	movefigure(target: Cell): void {
+		super.movefigure(target);
+	}
 }
+//52.53
